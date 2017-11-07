a19f37343ba444e8d7c923880fc2e7ac08bf3cd6
Allison Shultz <allisonjshultz@gmail.com>
2017-11-07 12:41:14 -0500

    Added statement to skip hogs with no -lnl

--- ComparativeGenomics/PAML_Parse/site_parser.py
+++ ComparativeGenomics/PAML_Parse/site_parser.py
@@ -28,44 +28,47 @@ def print_results(results, handle, model, treenum):
             for modelnum in sorted(res.get('NSsites',{}).keys(), key=int):
                 #fixed options
                 res_lnl = res.get('NSsites', {}).get(modelnum, {}).get('lnL')
-                res_treelen = res.get('NSsites', {}).get(modelnum, {}).get('tree length')
-                res_kappa = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('kappa')
-                #what to get depends on the model number
-                if modelnum == 0:
-                    #m0 case
-                    res_omega = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('omega')
-                elif modelnum == 1 or modelnum == 2:
-                    #m1/m2 case, use site classes 
-                    res_omega = None
-                    res_omegas = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('site classes', {})
-                    try:
-                        for sc in sorted(res_omegas.keys(), key=int):
-                            if res_omega is None:
-                                res_omega = str(round(res_omegas[sc]['proportion'],3)) + ":" + res_omegas[sc]['omega']
-                            else:
-                                res_omega += "," + str(round(res_omegas[sc]['proportion'],3)) + ":" + res_omegas[sc]['omega']
-                    except AttributeError:
-                        print(hog)
-                        continue
-                elif modelnum == 7:
-                    #m7 case
-                    m7_p = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p')),3))
-                    m7_q = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('q')),3))
-                    res_omega = m7_p + "/" + m7_q
-                elif modelnum == 8:
-                    try:
-                        m8_p = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p')),3))
-                        m8_q = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('q')),3))
-                        m8_p0 = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p0')),3))
-                        m8_p1 = str(round(1 - float(m8_p0),3))
-                        m8_w = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('w')),3))
-                        res_omega = m8_p0 + ":" + m8_p + "/" + m8_q + "," + m8_p1 + ":" + m8_w
-                    except TypeError:
-                        pass
+                if res_lnl == None:
+                	pass
                 else:
-                    pass
-                    
-                print(hog, model, treenum, trees.get('is_species_tree'), trees.get('original'), modelnum, res_lnl, res_treelen, res_kappa, res_omega, sep="\t", end="\n", file=handle)
+					res_treelen = res.get('NSsites', {}).get(modelnum, {}).get('tree length')
+					res_kappa = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('kappa')
+					#what to get depends on the model number
+					if modelnum == 0:
+						#m0 case
+						res_omega = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('omega')
+					elif modelnum == 1 or modelnum == 2:
+						#m1/m2 case, use site classes 
+						res_omega = None
+						res_omegas = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('site classes', {})
+						try:
+							for sc in sorted(res_omegas.keys(), key=int):
+								if res_omega is None:
+									res_omega = str(round(res_omegas[sc]['proportion'],3)) + ":" + res_omegas[sc]['omega']
+								else:
+									res_omega += "," + str(round(res_omegas[sc]['proportion'],3)) + ":" + res_omegas[sc]['omega']
+						except AttributeError:
+							print(hog)
+							pass
+					elif modelnum == 7:
+						#m7 case
+						m7_p = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p')),3))
+						m7_q = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('q')),3))
+						res_omega = m7_p + "/" + m7_q
+					elif modelnum == 8:
+						try:
+							m8_p = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p')),3))
+							m8_q = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('q')),3))
+							m8_p0 = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p0')),3))
+							m8_p1 = str(round(1 - float(m8_p0),3))
+							m8_w = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('w')),3))
+							res_omega = m8_p0 + ":" + m8_p + "/" + m8_q + "," + m8_p1 + ":" + m8_w
+						except TypeError:
+							pass
+					else:
+						pass
+					
+					print(hog, model, treenum, trees.get('is_species_tree'), trees.get('original'), modelnum, res_lnl, res_treelen, res_kappa, res_omega, sep="\t", end="\n", file=handle)
  
 
 def main():
