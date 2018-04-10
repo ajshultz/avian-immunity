178295ca953ab5512bb22be87470744244ec9692
Allison Shultz <allisonjshultz@gmail.com>
2018-04-10 11:59:57 -0400

    Reworked with original annotation, cleaned up

--- ComparativeGenomics/PAML_Analyze/05_Mammal_Bird_Gene_Set_Comparisons.R
+++ ComparativeGenomics/PAML_Analyze/05_Mammal_Bird_Gene_Set_Comparisons.R
@@ -21,24 +21,11 @@ mammal_annot <- bip %>%
   full_join(vip, by="ensID") %>%
   mutate(bip = !is.na(bip), vip=!is.na(vip), pip=!is.na(pip))
 
-#Read in chicken to human ensembl ID conversion, join annotation dataset.
-#ggal_hsap <- read_csv("05_input_mammal_data/galgal_ens_to_human.txt", col_names = c("ggalEnsId", "ensID", "confidence"))
-
-#Join annotation dataset and ensembl ID conversion, create bip, pip and vip tibbles with only those genes that fall into each category.
-#ggal_hsap <- full_join(mammal_annot, ggal_hsap, by="ensID") %>%
-#  mutate(bip = !is.na(bip), vip=!is.na(vip), pip=!is.na(pip))
-#ggal_annot <- ggal_hsap %>% dplyr::select(ggalEnsId, confidence, bip, pip, vip) %>% filter(!is.na(ggalEnsId)) %>% group_by(ggalEnsId) %>% summarize(bip = sum(bip)>0, vip=sum(vip)>0, pip=sum(pip)>0)
-
-#Translate chicken ensembl numbers to NCBI numbers, join to annotation data and write to file
-#ens_ncbi <- read_csv("05_input_mammal_data/galgal_ens_to_ncbi.txt", col_names = c("ggalEnsId", "entrezID", "name"))
-#ggal_annot <- left_join(ggal_annot, ens_ncbi)
-#write_csv(ggal_annot, "05_output_bird_mammal_comparison_results/ggal_alt_annotation.csv")
-
 #Apply the vip, bip and pip annotations to the hogs in our dataset, then add them back into the full dataset
 hog_annot <- all_res_sp_zf_hs %>%
-  right_join(mammal_annot,by=c("hsapiens_homolog_ensembl_gene" = "ensID")) %>%
+  right_join(mammal_annot,by=c("ensembl_gene_id_hs" = "ensID")) %>%
   group_by(hog) %>%
-  select(hog,entrezgene,bip,vip,pip) %>%
+  dplyr::select(hog,entrezgene,bip,vip,pip) %>%
   summarize(bip = sum(bip)>0, vip=sum(vip)>0, pip=sum(pip)>0)
 
 all_res_sp_zf_hs <- all_res_sp_zf_hs %>%
@@ -64,57 +51,39 @@ primate<-read_tsv("05_input_mammal_data/primate-9sp-data.txt") %>%
 
 mammal_select <- full_join(enard, primate)
 
-#mammal_select <- full_join(enard, primate) %>%
-#  left_join(ggal_hsap)
-
-#Remove missing data (replace by non-sig results, and filter non one-to-one mapping by choosing the minimum busted pvalue, etc.)
-ggal_mammal_comp <- mammal_select %>%
-  ungroup %>%
-  dplyr::select(ggalEnsId, BUSTED:paml_sig) %>%
-  filter(!is.na(ggalEnsId)) %>% 
-  replace_na(list(BUSTED=1, bs_ct=0, psr=0, paml_sig=0)) %>% 
-  group_by(ggalEnsId) %>% 
-  summarize(bustedp = min(BUSTED, na.rm=T), bs_ct = max(bs_ct, na.rm=T), psr = max(psr, na.rm=T), paml_sig = max(paml_sig, na.rm=T))
-
 #Add hog designations to mammal data, remove missing mammal data (replace by non-sig results, and filter non one-to-one mapping by choosing the minimum busted pvalue, etc.)
-mammal_clean 
-all_res_sp_zf_hs %>%
-  select(ensembl_gene_id,hsapiens_homolog_ensembl_gene) %>%
-  right_join(mammal_select,by=c("hsapiens_homolog_ensembl_gene" = "ensID")) %>%
-  filter(!is.na(ensembl_gene_id)) %>%
+mammal_clean <- all_res_sp_zf_hs %>%
+  ungroup() %>%
+  dplyr::select(hog,ensembl_gene_id,ensembl_gene_id_hs) %>%
+  right_join(mammal_select,by=c("ensembl_gene_id_hs" = "ensID")) %>%
+  filter(!is.na(hog)) %>%
   replace_na(list(BUSTED=1, bs_ct=0, psr=0, paml_sig=0)) %>% 
-  group_by(ensembl_gene_id) %>% 
+  group_by(hog) %>% 
   summarize(bustedp = min(BUSTED, na.rm=T), bs_ct = max(bs_ct, na.rm=T), psr = max(psr, na.rm=T), paml_sig = max(paml_sig, na.rm=T))
   
 
-#Add in ensembl IDs and write to file.
-ggal_mammal_comp <- ggal_mammal_comp %>%
-  left_join(ens_ncbi)
-
-ggal_mammal_comp %>%
-  write_csv("05_output_bird_mammal_comparison_results/ggal_mammal_comp.csv")
-
 #Combine bird and mammal datasets, only keep BUSTED results to ensure direct comparisons with mammals.
 birds<-all_res_sp_zf_hs %>%
-  dplyr::select(entrezgene,hog,pval_busted:FDRPval_busted)
+  dplyr::select(entrezgene,entrezgene_hs,hog,pval_busted:FDRPval_busted,bip,vip,pip)
 
-imm<-full_join(ggal_mammal_comp, ggal_annot) %>%
-  filter(!is.na(entrezID)) %>%
-  full_join(birds, by=c("entrezID" = "entrezgene")) %>% filter(!is.na(hog))
+imm<-full_join(mammal_clean, birds) %>%
+  filter(!is.na(hog)) %>%
+  distinct(hog, .keep_all=TRUE) %>%
+  filter(!is.na(bustedp), !is.na(pval_busted))
 
-imm <- imm %>% distinct(hog, .keep_all=TRUE)
 
-imm %>% filter(vip)
+#Get -log10 pvalues and qvalues (only consider genes in both datasets)
+imm <-imm %>%
+  mutate(mammal_logp = -1 * log10(bustedp+2.22e-16), bird_logp = -1 * log10(pval_busted+2.22e-16)) %>%
+  mutate(mammal_q = p.adjust(bustedp, method="BH"), bird_q = p.adjust(pval_busted, method="BH"))
+
 
 #############################################################################
 ###Use combined dataset to make inferences about similarity in selection###
 #############################################################################
 
-#Get -log10 pvalues.
-imm<-imm %>% mutate(mammal_logp = -1 * log10(bustedp+2.22e-16), bird_logp = -1 * log10(pval_busted+2.22e-16))
-
 #Plot correlations of p-values between birds and mammals, for each interaction-type.
-vip_pval_plot <- imm %>% filter(!is.na(mammal_logp), !is.na(bird_logp)) %>%
+vip_pval_plot <- imm %>%
   ggplot(aes(x=bird_logp, y=mammal_logp, col=vip, fill=vip)) +
   geom_point(size=0.5,alpha=0.5) + geom_smooth(method="lm") +
   theme_bw() +
@@ -140,24 +109,7 @@ plot_grid(vip_pval_plot, bip_pval_plot, pip_pval_plot,ncol=3)
 ggsave(filename = "05_output_bird_mammal_comparison_results/mammal_bird_pval_plots.pdf",width = 8, height=3)
 
 
-#imm %>% filter(mammal_logp > 8, bird_logp > 8) %>% dplyr::select(entrezID, vip) %>% print.data.frame
-
-#boxplot(imm$bird_logp ~ imm$paml_sig)
-
-#imm %>% dplyr::filter(paml_sig == 1, bird_logp < 4) %>% dplyr::select(name) %>% print.data.frame()
-
-#boxplot(imm$bird_logp ~ imm$vip, notch=T)
-
-#Create new q-values considering the genes in this overlap dataset
-imm <- imm %>%
-  mutate(mammal_q = p.adjust(bustedp, method="BH"), bird_q = p.adjust(pval_busted, method="BH"))
-
-
-
-
-
-#Numbers of genes overlapping at q<0.1 - q<-0.0001 and fisher's exact tests for significance.
-#imm <- imm %>% mutate(comp_busted = (mammal_q < 0.01) + (bird_q < 0.01))
+#Calculate numbers of genes overlapping at q<0.1 - q<-0.0001 and Fisher's exact tests for significance.
 #Create column to identify non-immune genes (not vips, bips, or pips)
 imm <- imm %>% mutate(non_immune = as.logical(vip == FALSE & bip == FALSE & pip == FALSE))
 
@@ -167,32 +119,39 @@ qval_res_list <- list()
 
 for (i in 1:length(qvals)){
   
-  comp_propsig <- matrix(nrow=5,ncol=12)
+  comp_propsig <- matrix(nrow=5,ncol=13)
   comp_propsig[,1] <- c("non-immune","vips","bips","pips","all_genes")
   
   #Fisher's exact tests
-  comp_propsig[1,2:9] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% with(., table(non_immune, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
-  comp_propsig[2,2:9] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% with(., table(vip, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
-  comp_propsig[3,2:9] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% with(., table(bip, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
-  comp_propsig[4,2:9] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% with(., table(pip, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
+  comp_propsig[1,2:9] <- imm %>% with(., table(non_immune, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
+  comp_propsig[2,2:9] <- imm %>% with(., table(vip, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
+  comp_propsig[3,2:9] <- imm %>% with(., table(bip, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
+  comp_propsig[4,2:9] <- imm %>% with(., table(pip, mammal_q < qvals[i] & bird_q < qvals[i])) %>% fisher.test %>% unlist
   comp_propsig[5,2:9] <- NA
   
   #proportions selected in both
-  comp_propsig[1,10:11] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% filter(non_immune == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
-  comp_propsig[2,10:11] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% filter(vip == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
-  comp_propsig[3,10:11] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% filter(bip == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
-  comp_propsig[4,10:11] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% filter(pip == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
-  comp_propsig[5,10:11] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
+  comp_propsig[1,10:11] <- imm %>% filter(non_immune == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
+  comp_propsig[2,10:11] <- imm %>% filter(vip == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
+  comp_propsig[3,10:11] <- imm %>% filter(bip == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
+  comp_propsig[4,10:11] <- imm %>% filter(pip == TRUE) %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
+  comp_propsig[5,10:11] <- imm %>% with(., prop.table(table(mammal_q < qvals[i] & bird_q < qvals[i])))
   
   comp_propsig[,12] <- qvals[i]
   
-  colnames(comp_propsig) <- c("class","p.value","conf.int1","conf.int2","estimated.odds.ratio","null.value.odds.ratio","alternative","method","data.name","prop_no","prop_sel_both","qval")
+  #number of genes in each category
+  comp_propsig[1,13] <- imm %>% filter(!is.na(mammal_q), !is.na(bird_q)) %>% filter(non_immune == TRUE) %>% summarize(n()) %>% pull
+  comp_propsig[2,13] <- imm %>% filter(vip == TRUE) %>% summarize(n()) %>% pull
+  comp_propsig[3,13] <- imm %>% filter(bip == TRUE) %>% summarize(n()) %>% pull
+  comp_propsig[4,13] <- imm %>% filter(pip == TRUE) %>% summarize(n()) %>% pull
+  comp_propsig[5,13] <- imm %>% summarize(n()) %>% pull
   
-  #Clean up, select relevant columsn
+  colnames(comp_propsig) <- c("class","p.value","conf.int1","conf.int2","estimated.odds.ratio","null.value.odds.ratio","alternative","method","data.name","prop_no","prop_sel_both","qval","n.genes")
+  
+  #Clean up, select relevant columns
   comp_propsig_clean <- comp_propsig %>%
     as.tibble %>%
     mutate(p.value=round(as.numeric(p.value),digits = 4),odds.ratio=round(as.numeric(estimated.odds.ratio),digits=2),prop_sel_both=round(as.numeric(prop_sel_both),digits=3), conf.lower=round(as.numeric(conf.int1),digits=3), conf.upper=round(as.numeric(conf.int2),digits=3)) %>%
-    dplyr::select(class,qval,odds.ratio,conf.lower,conf.upper,p.value,prop_sel_both)
+    dplyr::select(class,qval,n.genes,odds.ratio,conf.lower,conf.upper,p.value,prop_sel_both)
 
   
   qval_res_list[[i]] <- comp_propsig_clean
@@ -202,28 +161,50 @@ qval_res <- qval_res_list %>% bind_rows
 
 write_csv(qval_res,path="05_output_bird_mammal_comparison_results/mammal_bird_prop_selected_test_allq_results.csv")
 
+#Create vector of asterisks to include in plots:
+qval_res <- qval_res %>%
+  mutate(sig = case_when(
+    is.na(p.value) ~ "",
+    p.value > 0.05 ~ "",
+    p.value <= 0.05 & p.value > 0.01 ~ "*",
+    p.value <= 0.01 & p.value > 0.001 ~ "**",
+    p.value <= 0.001 ~ "***"
+  ))
 
-ggplot(qval_res,aes(factor(qval,levels=c("0.1","0.01","0.001","1e-04")),prop_sel_both,fill=factor(class,levels=c("all_genes","non-immune","vips","bips","pips")))) + geom_bar(stat = "identity",position="dodge") +
+
+prop_sel_plot <- qval_res %>%
+  filter(class != "all_genes") %>%
+  ggplot(aes(factor(qval,levels=c("0.1","0.01","0.001","1e-04")),prop_sel_both,fill=factor(class,levels=c("all_genes","non-immune","vips","bips","pips")))) +
+  geom_bar(stat = "identity",position=position_dodge()) +
+  geom_text(aes(factor(qval,levels=c("0.1","0.01","0.001","1e-04")),label=sig),position=position_dodge(width=0.9),size=10) +
   scale_x_discrete(labels=c("0.1","0.01","0.001","0.0001")) +
   scale_fill_discrete(name="class") +
   xlab("q-value") +
-  ylab("proportion selected birds + mammals")
+  ylab("proportion selected birds + mammals") +
+  labs(subtitle="* = p < 0.05, ** = p < 0.01, *** = p < 0.001")
   
+odds_ratio_plot <- qval_res %>%
+  filter(class != "all_genes") %>%
+  ggplot(aes(factor(qval,levels=c("0.1","0.01","0.001","1e-04")),odds.ratio,col=factor(class,levels=c("all_genes","non-immune","vips","bips","pips")))) +
+  geom_point(size=4,position=position_dodge(width=0.9)) +
+  geom_linerange(aes(factor(qval,levels=c("0.1","0.01","0.001","1e-04")),ymin=conf.lower,ymax=conf.upper,col=factor(class,levels=c("all_genes","non-immune","vips","bips","pips"))),position=position_dodge(width=0.9),size=1.5) +
+  geom_hline(aes(yintercept = 1)) +
+  scale_x_discrete(labels=c("0.1","0.01","0.001","0.0001")) +
+  scale_color_discrete(name="class") +
+  xlab("q-value") +
+  ylab("odds ratio") +
+  scale_y_continuous(labels=function(label) sprintf('%6.0f', label))
+
+plot_grid(prop_sel_plot, odds_ratio_plot, ncol=1)
+ggsave(filename = "05_output_bird_mammal_comparison_results/mammal_bird_prop_sel_both.pdf",width = 10, height=10)
 
 
-#Create barplot
-ggplot(comp_propsig_clean, aes(factor(class,levels=c("all","vips","bips","pips")), prop_sel_both)) +
-  geom_col(fill=c("gray30", "red2", "red2", "red2")) +
-  theme_classic() +
-  ylab("proportion selected") +
-  xlab("class")
-ggsave("05_output_bird_mammal_comparison_results/proportion_selected_birds_mammals.pdf")
+
+
 
 
 ###Compare proportions of genes selected in both mammals and birds at different q-value cutoffs
 #Plot bird vs. mammal log pvals, color by sig category
-#imm %>% filter(!is.na(mammal_logp), !is.na(bird_logp)) %>% mutate(plotcol = paste0(mammal_q<0.01, bird_q<0.01)) %>% ggplot(aes(x=bird_logp, y=mammal_logp, col=plotcol)) + geom_point()
-
 
 propsig_qvals <- matrix(nrow=4,ncol=11)
 propsig_qvals[,1] <- c(0.1,0.01,0.001,0.0001)
@@ -277,18 +258,3 @@ imm %>% filter(!is.na(mammal_logp), !is.na(bird_logp), !is.na(entrezID), bird_q 
 imm %>% filter(!is.na(mammal_logp), !is.na(bird_logp), !is.na(entrezID), mammal_q < 0.01, bird_q > .99) %>% select(entrezID) %>% write_tsv("mammal_specific.txt", col_names = F)
 imm %>% filter(!is.na(mammal_logp), !is.na(bird_logp), !is.na(entrezID), mammal_q > 0.99, bird_q < 0.01) %>% select(entrezID) %>% write_tsv("bird_specific.txt", col_names = F)
 
-
-
-sig_both <- imm %>% filter(!is.na(mammal_logp), !is.na(bird_logp)) %>%
-  filter(bird_q < 0.0001 & mammal_q < 0.0001) 
-
-
-
-
-
-
-
-
-#Create column to ID hogs signficant in all site tests
-hyphy_paml_res$sig <- rep("not_sig",nrow(hyphy_paml_res))
-hyphy_paml_res[all_selected_hogs,"sig"] <- "sig"
