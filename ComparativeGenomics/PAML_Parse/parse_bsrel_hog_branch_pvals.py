c45238ab220cd1f31252fa192e84eaaf073874b8
Allison Shultz <allisonjshultz@gmail.com>
2018-04-23 15:47:01 -0400

    Parse bsrel data to get pvals for each branch

---
+++ ComparativeGenomics/PAML_Parse/parse_bsrel_hog_branch_pvals.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+#For use with Python 3.
+
+import re
+import sys
+import os
+import argparse
+import csv
+
+#Takes the first column of an input csv file and returns as a list, skips first line
+def grab_sp(spnamesfile):
+    spnames = open(spnamesfile,"r")
+    sp = []
+    count = 1
+    for line in spnames:
+        if count == 1:
+            count += 1
+        else:
+            line = line.strip()
+            line = line.split(",")
+            sp.append(line[0])
+            count += 1
+    spnames.close()
+    return(sp)
+
+
+def pull_sp_pval(res_list,species,type="pval"):
+    if type == "pval":
+        res = ([float(res[6]) for res in res_list if species in res[0]])
+    elif type == "pvalholm":
+        res = ([float(res[7]) for res in res_list if species in res[0]])
+    else:
+        print("pval type must be pval or pvalholm")
+    if len(res) < 1:
+        return('NA')
+    else:
+        return(str(min(res)))
+    
+
+def main():
+    #Get config file from arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hog", help="hog to analyze")
+    parser.add_argument("--results_file", help="bsrel resultsfile to analyze")
+    parser.add_argument("--spnames_file", help="file of species to analyze, expects code, or abbreviation to be in the first column")
+    parser.add_argument("--pval_type", help="pval (raw pvalue) or pvalholm (FDR adjusted)")
+
+    args = parser.parse_args()
+    
+    hog = args.hog
+    results_file = args.results_file
+    spnames_file = args.spnames_file
+    pval_type = args.pval_type
+    
+    if pval_type != "pval" and pval_type != "pvalholm":
+        print("pval_type must be pval (raw pvalue) or pvalholm (FDR adjusted)")
+        quit()
+    
+    #Get list of species names
+    sp = grab_sp(spnames_file)
+    
+    #Look for the presence of a results file for a given hog, if not present, print NAs
+    
+    all_na = ["NA"]*len(sp)
+    
+    if not os.path.isfile(results_file):
+        no_data = "\t".join(all_na)
+        print('%s\t%s'%(hog,no_data))
+        quit()
+
+    #now read results into list
+    with open(results_file, 'r') as f:
+        reader=csv.reader(f)
+        res_list=list(reader)
+
+    pval_list = []
+    pvalholm_list = []
+
+    for i in range(0,len(sp)):
+        species = sp[i]
+        pval_list.append(pull_sp_pval(res_list,species,type=pval_type))
+
+    pvals = "\t".join(pval_list)
+    print('%s\t%s'%(hog,pvals))
+
+
+if __name__ == "__main__":
+    main()
---
+++ ComparativeGenomics/PAML_Parse/parse_hyphy_bs-rel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##HYPHY SCRIPT##
+##all data in /n/holylfs/LABS/edwards_lab/tsackton/HyPhy/DATA
+
+WORKDIR=$(pwd -P)
+SPNAMES_FILE=$1
+DATE=$(date +%F)
+
+for HOG in $(cat all_hogs)
+do
+    SUBDIRNUM=`expr ${HOG##HOG} % 100` 
+    printf -v SUBDIR "%04d" $SUBDIRNUM
+    RUNDIR=$SUBDIR/$HOG
+    OUTPUT1=/n/holylfs/LABS/edwards_lab/tsackton/ratite_work/06_protein_coding/HyPhy/BS-REL/$RUNDIR/tree1/$HOG.aBSREL.OUT
+    OUTPUT2=/n/holylfs/LABS/edwards_lab/tsackton/ratite_work/06_protein_coding/HyPhy/BS-REL/$RUNDIR/tree2/$HOG.aBSREL.OUT
+
+    RESULTS_TREE1_PVAL=$(python parse_bsrel_hog_branch_pvals.py --hog $HOG --results_file $OUTPUT1 --spnames_file $SPNAMES_FILE --pval_type pval)
+    RESULTS_TREE1_PVALHOLM=$(python parse_bsrel_hog_branch_pvals.py --hog $HOG --results_file $OUTPUT1 --spnames_file $SPNAMES_FILE --pval_type pvalholm)
+    RESULTS_TREE2_PVAL=$(python parse_bsrel_hog_branch_pvals.py --hog $HOG --results_file $OUTPUT2 --spnames_file $SPNAMES_FILE  --pval_type pval)
+    RESULTS_TREE2_PVALHOLM=$(python parse_bsrel_hog_branch_pvals.py --hog $HOG --results_file $OUTPUT2 --spnames_file $SPNAMES_FILE --pval_type pvalholm)
+
+    echo -e "pval\ttree1\t$RESULTS_TREE1_PVAL" >> bsrel_res_parsed_pvals_${DATE}.txt
+    echo -e "pvalholm\ttree1\t$RESULTS_TREE1_PVAL" >> bsrel_res_parsed_pvals_${DATE}.txt
+    echo -e "pval\ttree2\t$RESULTS_TREE2_PVALHOLM" >> bsrel_res_parsed_pvals_${DATE}.txt
+    echo -e "pvalholm\ttree2\t$RESULTS_TREE2_PVALHOLM" >> bsrel_res_parsed_pvals_${DATE}.txt
+done
